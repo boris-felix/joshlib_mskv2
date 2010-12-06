9e1e61621d5dc5d18ddc35897aa1b29f971e910f
dascritch <xavier@dascritch.com>
2010-12-06 17:55:22 +0100

    Insertion de textes explications à la lecture vidéo , sans lancer d'events

--- js/josh/uielements/video/all.js
+++ js/josh/uielements/video/all.js
@@ -8,11 +8,23 @@
             this.app.subscribe("menuChange",function(ev,data) {
               
                 //Filter only video menu items on the "current" register
-                if (data[0]=="current") {
+                if (data[0]=="current")
+				{
+					
                     
-                    if (self.app.menu.getData(data[1]).type=="video") {
-                        
+                    if (self.app.menu.getData(data[1]).type=="video")
+					{
                         self.play(self.app.menu.getData(data[1]));
+						
+						if (self.options.HtmlTags !== undefined)
+						{
+							for(var el in self.options.HtmlTags)
+							{
+								var tag =  self.options.HtmlTags[el].split(/\s/,2)[0];
+								$(tag,'#'+self.htmlId).remove();
+								$('<'+self.options.HtmlTags[el]+'>'+ self.app.menu.getData(data[1])[el]+'</'+tag+'>').appendTo('#'+self.htmlId);
+							}
+						}
                     }
                     
                 }
