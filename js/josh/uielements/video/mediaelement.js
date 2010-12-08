31672736904a13b98fd7b306a5e3ebc667064fa6
dascritch <xavier@dascritch.com>
2010-12-08 15:35:36 +0100

    ah c'est vraiment rageant

--- js/josh/uielements/video/mediaelement.js
+++ js/josh/uielements/video/mediaelement.js
@@ -7,14 +7,16 @@
 		    $("#"+this.htmlId)[0].innerHTML="";
 				// ça ne devrait même pas exister : on cherche une id, on a qu'un seul élément en rztour.
 			
-			$("#"+this.htmlId)[0].innerHTML = "<video id='"+this.htmlId+"_video' src='"+options["url"]+"'  width='100%' height='100%' controls autoplay='true' autobuffer preload poster='"+options["image"]+"' />";			  // NON ! Ne pas ! ne pas ! ne jamais ! width='100%' height='100%' 
+			$("#"+this.htmlId)[0].innerHTML = "<video id='"+this.htmlId+"_video' src='"+options["url"]+"' controls autoplay='true' autobuffer preload poster='"+options["image"]+"' />";			  
+				// width='100%' height='100%'  
+					/// NON ! Ne pas ! ne pas ! ne jamais ! le style doit se décider en css ou après coup dynamiquement. Jamais en dur de manière imparamétrable : on ne s'en sort jamais sinon.
             
-console.info('video elem ','#'+this.htmlId+"_video",$('#'+this.htmlId+"_video")/*[0]*/)
+console.info('video elem ',this)
             
             this.mejs = new MediaElementPlayer($('#'+this.htmlId+"_video")[0],{
                 pluginPath:"/swf/",
-                videoWidth:$('#'+this.htmlId+"_video").width()+1,
-                videoHeight:$('#'+this.htmlId+"_video").height(),
+                videoWidth: (typeof this.options['width'] !== 'undefined') ? this.options['width'] : $('#'+this.htmlId+"_video").parent().width()+1,
+                videoHeight: (typeof this.options['height'] !== 'undefined') ? this.options['height'] : $('#'+this.htmlId+"_video").parent().height(),
                 //enablePluginDebug:true,
                 success:function(me) {
                     console.log("MED SUCCESS ",me);
