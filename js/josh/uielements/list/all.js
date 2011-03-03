4b7c51e14921f4aed0a6b627bdff401051c67cec
Idir IBOUCHICHENE <merry@merry-laptop.(none)>
2011-03-03 15:59:54 +0100

    certains bugs sont resolus

--- js/josh/uielements/list/all.js
+++ js/josh/uielements/list/all.js
@@ -284,24 +284,117 @@
 						{
 							moveObj[prop] = Math.min(0, width - left);
 							// ajout debug
-							// alert(moveObj[prop] + "-" + "Passage 1");  // <==============================
+							alert(moveObj[prop] + "-" + "Passage 1");  // <==============================
 							// end
+							
+							/*
+							if (q == false && moveObj[prop] == 0)
+								{
+									var p = moveObj[prop];
+									moveObj[prop] = p + o;
+									alert("0 - 1");
+									
+									if (moveObj[prop] == 6000)
+										{
+											alert("0 - 2");
+											moveObj[prop] = (moveObj[prop]-1220);
+										}
+									else
+										{
+											moveObj[prop] - o;
+										}
+									var q;
+								}
+							else if (q)
+								{
+									var q = false;
+								}
+								
+							
+							if (moveObj[prop] == 0 || prop)
+								{
+									moveObj[prop] - 1220;
+								}
+							else
+								{
+									alert('else');
+								}
+							
+								
+							if (moveObj[prop] == 0)
+								{
+									alert('0');
+									
+									if (animate) {
+										list.stop().animate(moveObj, 200);
+										alert('1');
+									} else {
+										list.stop().css(moveObj);
+										alert('2');
+									}
+								}
+							
+							*/
+							
+							if (moveObj[prop] == 0 && left0 == true)
+								{
+									if (left1 == 4)
+										{
+											moveObj[prop] = moveObj[prop] - 4820;
+											left1 = 3;
+										}
+									else if (left1 == 3)
+										{
+											moveObj[prop] = moveObj[prop] - 3620;
+											left1 = 2;
+										}
+									else if (left1 == 2)
+										{
+											moveObj[prop] = moveObj[prop] - 2420;
+											left1 = 1;
+										}
+									else if (left1 == 1)
+										{
+											moveObj[prop] = moveObj[prop] - 1220;
+											left1 = 0;
+										}
+									else if (left1 == 0)
+										{
+											moveObj[prop] = moveObj[prop] + 20;
+											left1 = 4;
+										}
+										
+									// left0 = false;
+									alert('false');
+								}
 						}
 					else if (left + width > totalPixels - safetyMargin) {
                         moveObj[prop] = width - left;
 						// ajout debug
-						// alert(moveObj[prop] + "-" + "Passage 2"); // <================================
+						alert(moveObj[prop] + "-" + "Passage 2"); // <================================
 						// end
 						
-						// alert(prop);
+						left0 = true;
+						
+						alert(prop);
+						/*
+						function att()
+							{
+								alert(moveObj[prop]);
+							}
+						
+						setInterval(att, 1000);
+						
+						il faudrait faire keyIsUp
+						*/
 						
 						if(moveObj[prop] == -1095 && prop == "right")
 							{
-								// alert('right - ' + prop);
+								alert('right - ' + prop);
 							}
 						else if(moveObj[prop] == -1095 && prop == "left")
 							{
-								// alert('left - ' + prop);
+								alert('left - ' + prop);
 							}
 						else if(moveObj[prop] == -614)
 							{
@@ -318,6 +411,42 @@
 						else if(moveObj[prop] == -1140)
 							{
 								moveObj[prop] = -5475;
+/*-------------------*/		}// AVANCE NIVEAU 2
+						else if(moveObj[prop] == -1272)
+							{
+								moveObj[prop] = -3600;
+/*-------------------*/		}// AVANCE NIVEAU 3
+						else if(moveObj[prop] == -1200 || moveObj[prop] == -1220)
+							{
+								left2 = false;
+								left1 = 0;
+							}
+						else if((moveObj[prop] == -1252 && left2 == false) || moveObj[prop] == -1232)
+							{
+								moveObj[prop] = -2400;
+								left2 = true;
+								left1 = 1;
+								left0 = true;
+							}
+						else if(moveObj[prop] == -1176 || moveObj[prop] == -1156)
+							{
+								moveObj[prop] = -3600;
+								left2 = false;
+								left1 = 2;
+								left0 = true;
+							}
+						else if(moveObj[prop] == -968 || moveObj[prop] == -948 || moveObj[prop] == -1252)
+							{
+								moveObj[prop] = -4800;
+								left2 = false; // ajout car bug
+								left1 = 3;
+								left0 = true;
+							}
+						else if(moveObj[prop] == -978 || moveObj[prop] == -958)
+							{
+								moveObj[prop] = -6000;
+								left1 = 4;
+								left0 = true;
 /*-------------------*/		}// RETOUR -----------------------------------------------------------
 						else if(moveObj[prop] == -4855)
 							{
@@ -390,197 +519,94 @@
 						else if(moveObj[prop] == -1283)
 							{
 								moveObj[prop] = -0;
-							}
-						
-						// alert(prop);
-						
-						/*
-						else if(prop = "left")
+/* ----------------- */		}// RETOUR 2
+						else if(moveObj[prop] == -5520)
 							{
-								// alert('je passe | left');
-								moveObj[prop] = 0;
+								// COME BACK 1
+								moveObj[prop] = -4300;
 							}
-						else if(prop = "right")
+						else if(moveObj[prop] == -5240)
 							{
-								// alert('je passe | right');
-								moveObj[prop] *= -1;
-								if (animate) {
-									list.stop().animate(moveObj, 200);
-								} else {
-									list.stop().css(moveObj);
-								}
-							}*/
-						
-							
-						/*if (Math.abs(moveObj[prop]) < 800 && Math.abs(moveObj[prop]) > 1){
-							if(moveObj[prop] < 0)
-								moveObj[prop] *= -1;
-							moveObj[prop] += 1500;
-							
-							if(moveObj[prop] < 1600){
-								moveObj[prop] += 500;
-								var pas = true;
+								moveObj[prop] = -0;
 							}
-							
-							moveObj[prop] *= -1;
-							
-							if(pas){
-								moveObj[prop] *= -1;
-								moveObj[prop] += 600;
-								moveObj[prop] *= -1;
-								// alert('passe');
-							}
-							
-						}
-						
-						if(moveObj[prop] == -1000 ||
-							moveObj[prop] == -2245){
-								moveObj[prop] -= 2500;
-								// alert('passe2');
-							}
-						
-						if(moveObj[prop] == -762 || 
-							moveObj[prop] == -1762 || 
-							moveObj[prop] == -793 ||
-							moveObj[prop] == -1793 ||
-							moveObj[prop] == -679 ||
-							moveObj[prop] == -1679 ||
-							moveObj[prop] == -991 ||
-							moveObj[prop] == -1033)
+						else if(moveObj[prop] == -5088)
 							{
-								moveObj[prop] -= 2300;
+								moveObj[prop] = -0;
 							}
-						
-						if(moveObj[prop] == -1146 || 
-							moveObj[prop] == -876)
-							moveObj[prop] -= 3700;
-						
-						if(moveObj[prop] <= -5100)
-							moveObj[prop] = 0;
-						else if(moveObj[prop] == -4855 ||
-							moveObj[prop] == -4876 ||
-							moveObj[prop] == -4179 ||
-							moveObj[prop] <= -4800 ||
-							moveObj[prop] == -4676 ||
-							moveObj[prop] == -4218 ||
-							moveObj[prop] == -4006 ||
-							moveObj[prop] == -3762 ||
-							moveObj[prop] == -3642 ||
-							moveObj[prop] == -3414 ||
-							moveObj[prop] == -3147 ||
-							moveObj[prop] == -3144 ||
-							moveObj[prop] == -2373 ||
-							moveObj[prop] == -3091){
-							// alert("passe3");
-							moveObj[prop] = Math.floor(moveObj[prop]/5);
-							// alert(moveObj[prop]);
-						}					
-						
-						if(moveObj[prop] == -4705)
-							moveObj[prop] = Math.floor(-moveObj[prop]/5);
-						
-						if(moveObj[prop] == -1054 ||
-							moveObj[prop] == -1024 ||
-							moveObj[prop] == -1096 ||
-							moveObj[prop] == -1130 ||
-							moveObj[prop] == -1092 ||
-							moveObj[prop] == -1083 ||
-							moveObj[prop] == -1067 ||
-							moveObj[prop] == -1174){
-							moveObj[prop] -= 4500;
-						}
-						
-						if(moveObj[prop] == -846)
-							moveObj[prop] -= 3500;
-							
-						if(moveObj[prop] == -341.8 ||
-							moveObj[prop] == -628 ||
-							moveObj[prop] == -2084 ||
-							moveObj[prop] == -2720 ||
-							moveObj[prop] == -1776 ||
-							moveObj[prop] == -801 ||
-							moveObj[prop] == -1120 ||
-							moveObj[prop] == -2603 ||
-							moveObj[prop] == -682 ||
-							moveObj[prop] == -317 ||
-							moveObj[prop] == -629 ||
-							moveObj[prop] == -803 ||
-							moveObj[prop] == -971 ||
-							moveObj[prop] == -802)
-							moveObj[prop] += 1000;
-						
-						// alert(moveObj[prop]);
-						
-						if(moveObj[prop] == -3091 || 
-							moveObj[prop] == -3464 ||
-							moveObj[prop] == -4709 ||
-							moveObj[prop] == -3960 ||
-							moveObj[prop] == -729 ||
-							moveObj[prop] == -683 ||
-							moveObj[prop] == -630 ||
-							moveObj[prop] == -4703 ||
-							moveObj[prop] == -1876 ||
-							moveObj[prop] == -1393 ||
-							moveObj[prop] == -1296)
-							moveObj[prop] = 0;
-							
-						if (moveObj[prop] == -1293)
+						else if(moveObj[prop] == -4872)
 							{
-								moveObj[prop] *= -1;
-								moveObj[prop] -= 1000; 
+								moveObj[prop] = -0;
 							}
-						
-						if(moveObj[prop] == -1583)
+						else if(moveObj[prop] == -4568)
 							{
-								moveObj[prop] = 3;
-								// alert(moveObj[prop]);
+								moveObj[prop] = -0;
 							}
-						
-						
-						if(moveObj[prop] == -1095 && typeof(maVariable)=='undefined')
+						else if(moveObj[prop] == -4427)
 							{
-								var maVariable = true; // -1095 a déjà été executé une fois
-								// il faut trouvé un moyen d'indiquer "2 ème fois, t'es égale à 0"
+								moveObj[prop] = -0;
 							}
-						else if(moveObj[prop] == -1095 && typeof(maVariable)==true)
+						else if(moveObj[prop] == -4195)
 							{
-								moveObj[prop] = 0;
+								moveObj[prop] = -0;
 							}
-							*/
-						/*
-						else if (Math.abs(moveObj[prop]) < 1510 && Math.abs(moveObj[prop]) >= 800){
-							if(moveObj[prop] < 0)
-								moveObj[prop] *= -1;
-							moveObj[prop] += 1000;
-							moveObj[prop] *= -1;
-							
-						}
+						else if(moveObj[prop] == -4004)
+							{
+								moveObj[prop] = -0;
+							}
+						else if(moveObj[prop] == -3749)
+							{
+								// COME BACK 2
+								moveObj[prop] = -2529;
+							}
+						else if(moveObj[prop] == -3576)
+							{
+								moveObj[prop] = -0;
+							}
+						else if(moveObj[prop] == -3360)
+							{
+								moveObj[prop] = -0;
+							}
+						else if(moveObj[prop] == -2928)
+							{
+								moveObj[prop] = -0;
+							}
+						else if(moveObj[prop] == -2662)
+							{
+								moveObj[prop] = -0;
+							}
+						else if(moveObj[prop] == -2452)
+							{
+								moveObj[prop] = -0;
+							}
+						else if(moveObj[prop] == -2280)
+							{
+								moveObj[prop] = -0;
+							}
+						else if(moveObj[prop] == -3147)
+							{
+								moveObj[prop] = -0;
+							}
+						else if(moveObj[prop] == -2007)
+							{
+								moveObj[prop] = -787;
+							}
+						else if(moveObj[prop] == -1848)
+							{
+								moveObj[prop] = -0;
+							}
+						else if(moveObj[prop] == -1632)
+							{
+								moveObj[prop] = -0;
+							}
+						else if(moveObj[prop] == -1416)
+							{
+								moveObj[prop] = -0;
+							}
 						
-						*/
+						alert(prop);
 						
-						// ajout debug
-						// alert(moveObj[prop] + "-" + "Passage 2.1"); // <===============================
-						// end
 
 					}
-					// ajout ligne
-					/*if (moveObj[prop] < 1900){
-						moveObj[prop] *= 1;
-					}
-					else if (moveObj[prop] > 1900){
-						moveObj[prop] *= -1
-					}
-					
-					if (Math.abs(moveObj[prop]) < 800 && Math.abs(moveObj[prop]) > 10){
-						moveObj[prop] += 600
-						moveObj[prop] *= -1;
-					}*/
-					// end						
-					
-					// ajout debug
-					// alert(moveObj[prop] + "-" + "Passage 3"); // <======================================
-					// end
-			
 
                     if (animate) {
                         list.stop().animate(moveObj, 200);
